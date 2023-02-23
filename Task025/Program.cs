4ba74e28a3f56773ab5ac5511d36ecbd0ad4519f
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-23 22:44:03 +0500

    Task025 - NaturalDegree

---
+++ Task025/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+int resultA = numberA;
+
+Console.Write($"Число A в натуральной степень числа B: {NaturalDegree(resultA)}");
+
+int NaturalDegree(int resultA)
+{
+    resultA = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        resultA = resultA * numberA;
+    }
+    return resultA;
+}
