cc7ce4a95bce67f74f8034684754bbc33a8d51c1
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-02-23 23:09:52 +0500

    Task029 - Random array

---
+++ Task029/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int N = number;
+
+int[] array = ZeroOneArray(N);
+
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] ZeroOneArray (int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
